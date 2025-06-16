a524aca32868df651a3b8e8eda67c48e19949e72
Rodolforaw <93884610+Rodolforaw@users.noreply.github.com>
2025-06-16 15:09:26 -0300

    Update sidebar.tsx

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -112,11 +112,6 @@ const SidebarProvider = React.forwardRef<
       window.addEventListener("keydown", handleKeyDown)
       return () => window.removeEventListener("keydown", handleKeyDown)
     }, [toggleSidebar])
-{
-  label: "Mapeamento de Obra",
-  href: "/mapeamento",
-  icon: Map,
-},
 
     // We add a state so that we can do data-state="expanded" or "collapsed".
     // This makes it easier to style the sidebar with Tailwind classes.
